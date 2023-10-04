f487292b180b7ec77c748537623c7c418819556e
Sofa <sofasven00@gmail.com>
2023-10-05 00:58:35 +0300

    Project created

---
+++ SPMAnimationApp/SPMAnimationApp/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  SPMAnimationApp
+//
+//  Created by Sofa on 5.10.23.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
